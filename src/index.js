56ede7ddbfa32989111843397187c2aed501645e
han-sen <michaelandhansen@gmail.com>
2020-02-13 20:47:54 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class Canvas extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button
+        style={{ backgroundColor: this.props.value }}
+        className="square"
+        onClick={() => this.props.onClick()}
+      ></button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  handleClick(i) {
+    const currentColor = document.getElementById("colorPicker").value;
+    const squares = this.state.squares.slice();
+    squares[i] = currentColor;
+    this.setState({ squares: squares });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const status = <input id="colorPicker" name="Color Picker" type="color" />;
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Canvas />, document.getElementById("root"));
