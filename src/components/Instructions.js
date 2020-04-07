e8fd2305fa047946754032cfe6c5a8ce86f0ede4
han-sen <michaelandhansen@gmail.com>
2020-04-06 23:43:34 -0400

    updated kind-of package version

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -4,11 +4,14 @@ class Instructions extends React.Component {
   render() {
     return (
       <div className="instructions_wrap">
-        <p>This is a React pixel art maker demo.</p>
-        <p>Toggle the grid on and off.</p>
-        <p>Use the 'detail' button to cycle between 1x, 2x, 3x square sizes</p>
-        <p> (note: this will erase any in place pixels)</p>
-        <p>Save your work!</p>
+        <p>&#8212; This is a React pixel art maker demo.</p>
+        <p>&#8212; Toggle the grid on and off.</p>
+        <p>
+          &#8212; Use the 'detail' button to cycle between 1x, 2x, 3x square
+          sizes
+        </p>
+        <p>(note: this will erase any in place pixels)</p>
+        <p>&#8212; Save your work!</p>
       </div>
     );
   }
