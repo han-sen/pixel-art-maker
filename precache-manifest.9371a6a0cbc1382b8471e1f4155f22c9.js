456b0a36d1d0d5fcdbb12cf681b80f14b2036f41
han-sen <michaelandhansen@gmail.com>
2020-05-01 16:08:53 -0400

    Updates

---
+++ precache-manifest.9371a6a0cbc1382b8471e1f4155f22c9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41286f1cbd7b22d177b286cb311e01a",
+    "url": "/pixel-art-maker/index.html"
+  },
+  {
+    "revision": "b6a68c40147c87a5b80b",
+    "url": "/pixel-art-maker/static/css/main.f96c7f64.chunk.css"
+  },
+  {
+    "revision": "a5474473dd51ba2bef88",
+    "url": "/pixel-art-maker/static/js/2.8b7a226b.chunk.js"
+  },
+  {
+    "revision": "6b58f50c32f9147edd9c978cbc52e417",
+    "url": "/pixel-art-maker/static/js/2.8b7a226b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a68c40147c87a5b80b",
+    "url": "/pixel-art-maker/static/js/main.92d72ced.chunk.js"
+  },
+  {
+    "revision": "735dfc64c087a709e7bb",
+    "url": "/pixel-art-maker/static/js/runtime-main.c15e31a6.js"
+  },
+  {
+    "revision": "16ff65e01d0c01c397f6234e91f3cb17",
+    "url": "/pixel-art-maker/static/media/brush-solid.16ff65e0.svg"
+  },
+  {
+    "revision": "20a39d62d91c5f5b5957ba7f120c4455",
+    "url": "/pixel-art-maker/static/media/clear.20a39d62.svg"
+  },
+  {
+    "revision": "b0f196a2686331079aa294e0f9c36245",
+    "url": "/pixel-art-maker/static/media/fill.b0f196a2.svg"
+  },
+  {
+    "revision": "968d69c290145c5a71ed7d76385bb997",
+    "url": "/pixel-art-maker/static/media/grid.968d69c2.svg"
+  },
+  {
+    "revision": "f3fa4609779468cf15bb736964db0390",
+    "url": "/pixel-art-maker/static/media/palette.f3fa4609.svg"
+  },
+  {
+    "revision": "3659541e229a2c0387a8987565e4118d",
+    "url": "/pixel-art-maker/static/media/save.3659541e.svg"
+  }
+]);
