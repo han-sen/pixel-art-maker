e9a4ca8c2c179e74ad50fc342727afda98e1cfef
Michael Hansen <michaelhansen@Michaels-MacBook-Pro.local>
2020-06-25 18:33:11 -0400

    updated build

---
+++ build/precache-manifest.6f61511c16bbbe488428a2052ffd9059.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4ed8d70189d6bd8828aad664afd7be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6036f2d377872a3b27ca",
+    "url": "/static/css/main.feddb11e.chunk.css"
+  },
+  {
+    "revision": "a5474473dd51ba2bef88",
+    "url": "/static/js/2.8b7a226b.chunk.js"
+  },
+  {
+    "revision": "6b58f50c32f9147edd9c978cbc52e417",
+    "url": "/static/js/2.8b7a226b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6036f2d377872a3b27ca",
+    "url": "/static/js/main.f2dba142.chunk.js"
+  },
+  {
+    "revision": "0778e2266596752e7524",
+    "url": "/static/js/runtime-main.d59db8c9.js"
+  },
+  {
+    "revision": "16ff65e01d0c01c397f6234e91f3cb17",
+    "url": "/static/media/brush-solid.16ff65e0.svg"
+  },
+  {
+    "revision": "20a39d62d91c5f5b5957ba7f120c4455",
+    "url": "/static/media/clear.20a39d62.svg"
+  },
+  {
+    "revision": "b0f196a2686331079aa294e0f9c36245",
+    "url": "/static/media/fill.b0f196a2.svg"
+  },
+  {
+    "revision": "968d69c290145c5a71ed7d76385bb997",
+    "url": "/static/media/grid.968d69c2.svg"
+  },
+  {
+    "revision": "f3fa4609779468cf15bb736964db0390",
+    "url": "/static/media/palette.f3fa4609.svg"
+  },
+  {
+    "revision": "3659541e229a2c0387a8987565e4118d",
+    "url": "/static/media/save.3659541e.svg"
+  }
+]);
